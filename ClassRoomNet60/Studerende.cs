4795bd0ed413adbdb637419843b23214e7869e0c
Hasibe Dönmez <hasi0048@easjdk.onmicrosoft.com>
2022-09-05 13:42:51 +0200

    Opgave 2

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+         private string _navn;
+
+        public string navn
+        {
+            get { return navn; }
+            set { navn = value; }
+        }
+
+
+        private int _fødselsmåned;
+
+        public string fødselsmåned
+        {
+            get { return fødselsmåned; }
+            set { fødselsmåned = value; }
+        }
+
+
+        private int _fødselsdag;
+
+        public string fødselsdag
+        {
+            get { return fødselsdag; }
+            set { fødselsdag = value; }
+        }
+
+
+        public Studerende()
+        {
+             
+        }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            _navn = navn;
+            _fødselsmåned = fødselsmåned;
+            _fødselsdag = fødselsdag;
+        }
+        
+
+    }
+}
